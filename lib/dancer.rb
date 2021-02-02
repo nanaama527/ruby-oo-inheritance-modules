0992be12c0fb7aa0242f9f67560ba2405b96289a
Nana <72888369+nanaama527@users.noreply.github.com>
2021-02-02 17:49:24 -0500

    done\

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+
+class Dancer
+    attr_accessor :name
+    
+    include Dance
+    extend MetaDancing
+
+    def intitialize(name)
+        @name = name
+    end
+end
