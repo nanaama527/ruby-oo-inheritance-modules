8680261a069eeefe6826ea5a5ffbf8a5a703872c
Nana <72888369+nanaama527@users.noreply.github.com>
2021-02-03 00:40:34 -0500

    commit tree ea44a6f0e74d06d7de4be067a94d8c32e54e8cf0

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
 
 class Kid
     attr_accessor :name
+
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
     
     include Dance
     extend MetaDancing
